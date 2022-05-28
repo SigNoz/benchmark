32491fff6b5c65320606fa243a606a9981dac0ca
Prashant Shahi <prashant@signoz.io>
2022-05-28 22:12:53 +0530

    chore(config): 🔧 update OTel config and hostmetrics dashboard files
    Signed-off-by: Prashant Shahi <prashant@signoz.io>

--- dashboards/hostmetrics/README.md
+++ dashboards/hostmetrics/README.md
@@ -9,7 +9,7 @@ You can use the following command to import the dashboard:
 
 ```bash
 curl -sL https://github.com/SigNoz/benchmark/raw/main/dashboards/hostmetrics/hostmetrics-import.sh \
-    | SIGNOZ_ENDPOINT="http://localhost:3301" HOSTNAME="test-instance-1" DASHBOARD_TITLE="HostMetrics Dashboard for test-instance-1" bash
+    | HOSTNAME="test-instance-1" DASHBOARD_TITLE="HostMetrics Dashboard for test-instance-1" bash
 ```
 
 Alternatively, you can use the clone the repository and run the script:
@@ -20,11 +20,12 @@ git clone https://github.com/SigNoz/benchmark.git
 cd dashboards/hostmetrics
 
 cat hostmetrics-import.sh \
-    | SIGNOZ_ENDPOINT="https://monitor.mysite.com:3301" HOSTNAME="test-instance-2" DASHBOARD_TITLE="HostMetrics Dashboard for test-instance-2" bash
+    | HOSTNAME="test-instance-2" DASHBOARD_TITLE="HostMetrics Dashboard for test-instance-2" bash
 ```
 
+To generate the dashboard, open SigNoz UI and upload the generated JSON.
+
 _*Notes:_
-- Set `SIGNOZ_ENDPOINT` variable to point to SigNoz frontend endpoint.
 - In case `HOSTNAME` variable is skipped, it will select the current machine hostname using the default shell variable `HOSTNAME`.
 - If `DASHBOARD_TITLE` variable is skipped, it will create title with the template: `HostMetrics Dashboard for <hostname>`.
 - Dashboard uuid is randomly generated. You can pass `DASHBOARD_UUID` variable to override.
--- dashboards/hostmetrics/hostmetrics-import.sh
+++ dashboards/hostmetrics/hostmetrics-import.sh
@@ -2,11 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")";
 
-# Set the default signoz endpoint if not set
-if [[ -z $SIGNOZ_ENDPOINT ]]; then
-    SIGNOZ_ENDPOINT="http://localhost:3301"
-fi
-
 # Set the hostname filter if hostname is passed
 if [[ -z $HOSTNAME ]]; then
     HOSTNAME="ALL"
@@ -25,19 +20,15 @@ if [[ -z $DASHBOARD_TITLE ]]; then
     DASHBOARD_TITLE="HostMetrics Dashboard - $HOSTNAME"
 fi
 
-# Setup sample apps into specified namespace
 (cat hostmetrics-template.json 2>/dev/null || curl -sL https://github.com/SigNoz/benchmark/raw/main/dashboards/hostmetrics/hostmetrics-template.json) | \
     DASHBOARD_UUID="${DASHBOARD_UUID}" \
     DASHBOARD_TITLE="${DASHBOARD_TITLE}" \
     HOSTNAME_FILTER="${HOSTNAME_FILTER}" \
     HOSTNAME_FILTER_COMMA="${HOSTNAME_FILTER_COMMA}" \
-    envsubst | curl --fail --silent --output /dev/null --show-error --location --request POST \
-    --header 'Accept: application/json, text/plain, */*' \
-    --header 'Content-Type: application/json' \
-    --data-binary @- "${SIGNOZ_ENDPOINT}/api/v1/dashboards"
+    envsubst > signoz-hostmetrics-${HOSTNAME}.json
 
 if [ $? -ne 0 ]; then
-    echo "❌ Failed to import Host Metrics dashboard"
+    echo "❌ Failed to generate Host Metrics dashboard"
 else
-    echo "✅ Succesfully imported Host Metrics dashboard"
+    echo "✅ Succesfully generated Host Metrics dashboard: signoz-hostmetrics-${HOSTNAME}.json"
 fi
