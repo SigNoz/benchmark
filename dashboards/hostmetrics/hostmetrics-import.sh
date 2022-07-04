5050de17a602d3222c296e8a89ac7fd8270d385d
Prashant Shahi <prashant@signoz.io>
2022-07-05 03:16:03 +0530

    chore(hostmetrics-import): 🔧 support importing dashboard using bearer token
    Signed-off-by: Prashant Shahi <prashant@signoz.io>

--- dashboards/hostmetrics/hostmetrics-import.sh
+++ dashboards/hostmetrics/hostmetrics-import.sh
@@ -10,25 +10,42 @@ else
     HOSTNAME_FILTER_COMMA="${HOSTNAME_FILTER},"
 fi
 
-# Set Dashboard UUID
-if [[ -z $DASHBOARD_UUID ]]; then
-    DASHBOARD_UUID="$(uuidgen || cat /proc/sys/kernel/random/uuid)"
-fi
-
 # Set Dashboard Title
 if [[ -z $DASHBOARD_TITLE ]]; then
     DASHBOARD_TITLE="HostMetrics Dashboard - $HOSTNAME"
 fi
 
-(cat hostmetrics-template.json 2>/dev/null || curl -sL https://github.com/SigNoz/benchmark/raw/main/dashboards/hostmetrics/hostmetrics-template.json) | \
-    DASHBOARD_UUID="${DASHBOARD_UUID}" \
-    DASHBOARD_TITLE="${DASHBOARD_TITLE}" \
-    HOSTNAME_FILTER="${HOSTNAME_FILTER}" \
-    HOSTNAME_FILTER_COMMA="${HOSTNAME_FILTER_COMMA}" \
-    envsubst > signoz-hostmetrics-${HOSTNAME}.json
+# Set the default signoz endpoint if not set
+if [[ -z $SIGNOZ_ENDPOINT ]]; then
+    SIGNOZ_ENDPOINT="http://localhost:3301"
+fi
+
+# If bearer token is set, generate dashboard JSON file
+# If bearer token not set, create dashboard JSON and import in SigNoz
+if [[ -z $BEARER_TOKEN ]]; then
+    FAIL_ACTION="generate"
+    SUCCESS_ACTION="generated"
+    (cat hostmetrics-template.json 2>/dev/null || curl -sL https://github.com/SigNoz/benchmark/raw/main/dashboards/hostmetrics/hostmetrics-template.json) | \
+        DASHBOARD_TITLE="${DASHBOARD_TITLE}" \
+        HOSTNAME_FILTER="${HOSTNAME_FILTER}" \
+        HOSTNAME_FILTER_COMMA="${HOSTNAME_FILTER_COMMA}" \
+        envsubst > signoz-hostmetrics-${HOSTNAME}.json
+else
+    FAIL_ACTION="import"
+    SUCCESS_ACTION="imported"
+    (cat hostmetrics-template.json 2>/dev/null || curl -sL https://github.com/SigNoz/benchmark/raw/main/dashboards/hostmetrics/hostmetrics-template.json) | \
+        DASHBOARD_TITLE="${DASHBOARD_TITLE}" \
+        HOSTNAME_FILTER="${HOSTNAME_FILTER}" \
+        HOSTNAME_FILTER_COMMA="${HOSTNAME_FILTER_COMMA}" \
+        envsubst | curl --fail --silent --output /dev/null --show-error --location --request POST \
+        --header 'Accept: application/json, text/plain, */*' \
+        --header 'Content-Type: application/json' \
+        --header "Authorization: Bearer ${BEARER_TOKEN}" \
+        --data-binary @- "${SIGNOZ_ENDPOINT}/api/v1/dashboards"
+fi
 
 if [ $? -ne 0 ]; then
-    echo "❌ Failed to generate Host Metrics dashboard"
+    echo "❌ Failed to ${FAIL_ACTION} Host Metrics dashboard"
 else
-    echo "✅ Succesfully generated Host Metrics dashboard: signoz-hostmetrics-${HOSTNAME}.json"
+    echo "✅ Succesfully ${SUCCESS_ACTION} Host Metrics dashboard: signoz-hostmetrics-${HOSTNAME}.json"
 fi
